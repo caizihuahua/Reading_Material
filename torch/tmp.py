a1dbc4030a15bce98f331f6bc45e4814222772dc
caizihuahua <bubu1919@163.com>
2022-07-09 21:25:40 +0800

    backup

---
+++ torch/tmp.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5) # 1 input image channel, 6 output channels, 5x5 square convolution kernel
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1   = nn.Linear(16*5*5, 120) # an affine operation: y = Wx + b
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2)) # Max pooling over a (2, 2) window
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2) # If the size is a square you can only specify a single number
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+    def num_flat_features(self, x):
+        size = x.size()[1:] # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+net = Net()
+
+params = list(net.parameters())
+print(len(params))
+print(params[0].size()) # conv1's .weight
+
+input = Variable(torch.randn(1, 1, 32, 32))
+out = net(input)
+net.zero_grad() # 对所有的参数的梯度缓冲区进行归零
+out.backward(torch.randn(1, 10)) # 使用随机的梯度进行反向传播
