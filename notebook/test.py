6256ce011ae3f7c975534e0379ae513d7c243024
caizihuahua <bubu1919@163.com>
2022-07-15 22:14:15 +0800

    backup

--- notebook/test.py
+++ notebook/test.py
@@ -7,7 +7,7 @@ import os,time,datetime,sys
 class PINN(tf.keras.Model):
     def __init__(
             self,
-            t_i,x_i,u_i,t_b,x_b,t_r,x_r,lb,ub,
+            t,x,u,t_r,x_r,lb,ub,
             in_dim,out_dim,width,depth,
             activ="tanh",w_init="glorot_normal",b_init="zeros",
             lr=1e-3,opt="Adam",
@@ -17,14 +17,12 @@ class PINN(tf.keras.Model):
         self.info_seed = info_seed
         # initial the data
         self.data_type = tf.float32
-        self.x_i = tf.convert_to_tensor(x_i,dtype=self.data_type)
-        self.t_i = tf.convert_to_tensor(t_i,dtype=self.data_type)
-        self.u_i = tf.convert_to_tensor(u_i,dtype=self.data_type)
-        self.x_b = tf.convert_to_tensor(x_b,dtype=self.data_type)
-        self.t_b = tf.convert_to_tensor(t_b,dtype=self.data_type)
+        self.x = tf.convert_to_tensor(x,dtype=self.data_type)
+        self.t = tf.convert_to_tensor(t,dtype=self.data_type)
+        self.u = tf.convert_to_tensor(u,dtype=self.data_type)
         # pde loss train point = inner + outer(initial+boundary)
-        t_r = tf.concat([t_i,t_b,t_r],axis=0)
-        x_r = tf.concat([x_i,x_b,x_r],axis=0)
+        t_r = tf.concat([t,t_r],axis=0)
+        x_r = tf.concat([x,x_r],axis=0)
         self.x_r = tf.convert_to_tensor(x_r,dtype=self.data_type)
         self.t_r = tf.convert_to_tensor(t_r,dtype=self.data_type)
         self.lb = tf.convert_to_tensor(lb,dtype=self.data_type)
@@ -87,36 +85,14 @@ class PINN(tf.keras.Model):
         r = tf.reduce_mean(tf.square(equ_1)+tf.square(equ_2))
         return r
 
-    def loss_ic(self):
-        u_nn_init = self.dnn(tf.concat([self.t_i,self.x_i],1))
-        loss_init = tf.reduce_mean(tf.square(self.u_i-u_nn_init))
-        return loss_init
-    def loss_bc(self):
-        # boundary xmin
-        xmin_index = [i for i,x in enumerate(self.x_b.numpy()) if x==self.lb[1]]
-        xmin_t = tf.convert_to_tensor([self.t_b[i] for i in xmin_index],dtype=self.data_type)
-        xmin_x = tf.convert_to_tensor([self.x_b[i] for i in xmin_index],dtype=self.data_type)
-        # boundary xmax
-        xmax_index = [i for i,x in enumerate(self.x_b.numpy()) if x==self.ub[1]]
-        xmax_t = tf.convert_to_tensor([self.t_b[i] for i in xmax_index],dtype=self.data_type)
-        xmax_x = tf.convert_to_tensor([self.x_b[i] for i in xmax_index],dtype=self.data_type)
-        # xmax_u = tf.convert_to_tensor([self.u_b[i] for i in xmax_index],dtype=self.data_type)
-        
-        with tf.GradientTape(persistent=True) as tp:
-            tp.watch(xmin_x)
-            tp.watch(xmax_x)
-            u_nn_xmin = self.dnn(tf.concat([xmin_t,xmin_x],1))
-            u_nn_xmax = self.dnn(tf.concat([xmax_t,xmax_x],1))
-        tmp1 = tp.gradient(u_nn_xmax,xmax_x)
-        tmp2 = tp.gradient(u_nn_xmin,xmin_x)
-        loss_b1 = tf.reduce_mean(tf.square(u_nn_xmax-u_nn_xmin))
-        loss_b2 = tf.reduce_mean(tf.square(tmp1-tmp2))
-        return loss_b1+loss_b2
+    def loss_icbc(self):
+        u_nn = self.dnn(tf.concat([self.t,self.x],1))
+        return tf.reduce_mean(tf.square(self.u-u_nn))
 
     @tf.function
     def grad_desc(self):
         with tf.GradientTape() as tp:
-            loss = self.loss_pde() + self.loss_ic() + self.loss_bc()
+            loss = self.loss_pde() + self.loss_icbc()
         grad = tp.gradient(loss,self.params)
         del tp
         self.optimizer.apply_gradients(zip(grad,self.params))
@@ -126,9 +102,6 @@ class PINN(tf.keras.Model):
         print(">>>>> training setting;")
         print("         # of epoch     :", epoch)
         print("         convergence tol:", tol)
-        self.loss_pde()
-        self.loss_ic()
-        self.loss_bc()
         t0 = time.time()
         for ep in range(epoch+1):
             ep_loss = self.grad_desc()
@@ -163,12 +136,13 @@ class PINN(tf.keras.Model):
         equ_2 = (rho_t*v + rho*v_t) + (rho*(2*v*v_x) +(v**2)*rho_x + p_x)
         gv = tf.square(equ_1)+tf.square(equ_2)
         return u,gv
+
 in_dim = 2
 out_dim = 3
 width = 20
 depth = 7
 
-epoch = 18000
+epoch = 12000
 tol = 1e-8
 
 N_0 = 50
@@ -195,24 +169,139 @@ print("tensorflow:", tf.__version__)
 print("rand seed :", info_seed)
 os.environ["PYTHONHASHSEED"] = str(info_seed)
 
-tmin, tmax =  0., 1.
-xmin, xmax = -1., 1.
+tmin, tmax = 0., 1.
+xmin, xmax = 0., 1.
 lb = tf.constant([tmin, xmin], dtype = tf.float32)
 ub = tf.constant([tmax, xmax], dtype = tf.float32)
 
 t_0 = tf.ones((N_0, 1), dtype = tf.float32) * lb[0]
 x_0 = tf.random.uniform((N_0, 1), lb[1], ub[1], dtype = tf.float32)
+u_0 = tf.convert_to_tensor([[1.4 if x>0.5 else 1.0 ,0.1,1.0] for x in x_0.numpy()],dtype=tf.float32)
+
 t_b = tf.random.uniform((N_b, 1), lb[0], ub[0], dtype = tf.float32)
 x_b = lb[1] + (ub[1] - lb[1]) * tf.keras.backend.random_bernoulli((N_b, 1), .5, dtype = tf.float32)
-t_r = tf.random.uniform((N_r, 1), lb[0], ub[0], dtype = tf.float32)
-x_r = tf.random.uniform((N_r, 1), lb[1], ub[1], dtype = tf.float32)
+u_b = tf.convert_to_tensor([[1.4 if x_b[i]<0.5+0.1*t_b[i] else 1.0 ,0.1 ,1.0] for i in range(len(x_b.numpy()))],dtype=tf.float32)
 
-u_0 = tf.convert_to_tensor([[1.+0.2*np.sin(np.pi*a),1.,1.] for a in x_0.numpy().flatten()],dtype=tf.float32)
+t = tf.concat([t_0,t_b],axis=0)
+x = tf.concat([t_0,t_b],axis=0)
+u = tf.concat([u_0,u_b],axis=0)
 
+t_r_rand = tf.random.uniform((N_r, 1), lb[0], ub[0], dtype = tf.float32)
+x_r_rand = tf.random.uniform((N_r, 1), lb[1], ub[1], dtype = tf.float32)
 
-pinn = PINN(t_0,x_0,u_0,t_b,x_b,t_r,x_r,lb,ub,
+t_r_clus = tf.random.uniform((N_r, 1), lb[0], ub[0], dtype = tf.float32)
+noise = 0.1
+a = 0.1
+b = 0.5
+aa = a*(1+np.random.randn()*noise)
+bb = b*(1+np.random.randn()*noise)
+cc = np.random.choice(range(t_r_clus.shape[0]),size=int(N_r*0.6), replace=False)
+x_r_clus =tf.convert_to_tensor([ [a*(1+np.random.randn()*noise)*t_r_clus[i].numpy()[0]+b*(1+np.random.randn()*noise)] if (i in cc) else [np.random.random()] for i in range(len(t_r_clus))],dtype=tf.float32)
+
+pinn_clus = PINN(
+            t,x,u,
+            t_r_clus,x_r_clus,
+            lb,ub,
             in_dim,out_dim,width,depth,
             act,w_init,b_init,
             lr,opt,info_seed)
 
-pinn.train(epoch,tol,info_freq)
+
+pinn_rand = PINN(t,x,u,t_r_rand,x_r_rand,lb,ub,
+            in_dim,out_dim,width,depth,
+            act,w_init,b_init,
+            lr,opt,info_seed)
+
+def plot_solution(X,u,savepath="./pics"):
+    lb = X.min(0)
+    ub = X.max(0)
+    x = np.linspace(lb[0],ub[0],200)
+    y = np.linspace(lb[1],ub[1],200)
+    x,y = np.meshgrid(x,y)
+    phi_1 = griddata(X,u[:,0].numpy().flatten(),(x,y),method="linear")
+    phi_2 = griddata(X,u[:,1].numpy().flatten(),(x,y),method="linear")
+    phi_3 = griddata(X,u[:,2].numpy().flatten(),(x,y),method="linear")
+
+    if not os.path.exists(savepath):
+        os.makedirs(savepath)
+    plt.figure(figsize=(6,8))
+    plt.imshow(phi_1,interpolation='nearest',cmap='rainbow',extent=[0,1,-1,1],origin="lower",aspect="auto")
+    plt.colorbar()
+    plt.title("density prediction")
+    plt.xlabel('t')
+    plt.ylabel('x')
+    plt.savefig(savepath+"/density prediction")
+
+    plt.figure(figsize=(6,8))
+    plt.imshow(phi_2,interpolation='nearest',cmap='rainbow',extent=[0,1,-1,1],origin="lower",aspect="auto")
+    plt.colorbar()
+    plt.title("velocity prediction")
+    plt.xlabel('t')
+    plt.ylabel('x')
+    plt.savefig(savepath+"/velocity prediction")
+
+    plt.figure(figsize=(6,8))
+    plt.imshow(phi_3,interpolation='nearest',cmap='rainbow',extent=[0,1,-1,1],origin="lower",aspect="auto")
+    plt.colorbar()
+    plt.title("pressure prediction")
+    plt.xlabel('t')
+    plt.ylabel('x')
+    plt.savefig(savepath+"/pressure prediction")
+    
+def plot_final(X,u,final_t,savepath="./pics"):
+    if not os.path.exists(savepath):
+        os.makedirs(savepath)
+    index = np.array([ [i,x[1]] for i,x in enumerate(TX) if x[0]==final_t])
+    density_clus = [u[0][:,0][int(i)] for i in index[:,0]]
+    density_rand = [u[0][:,0][int(i)] for i in index[:,0]]
+    velocity_clus = [u[1][:,1][int(i)] for i in index[:,0]]
+    velocity_rand = [u[1][:,1][int(i)] for i in index[:,0]]
+    pressure_clus = [u[1][:,2][int(i)] for i in index[:,0]]
+    pressure_rand = [u[1][:,2][int(i)] for i in index[:,0]]
+    xx = index[:,1]
+
+    fig1,ax1 = plt.subplots(1)
+    ax1.set_title(f"density(t={final_t})")
+    ax1.set_xlabel('x')
+    ax1.set_ylabel('density')
+    ax1.plot(xx,density_clus,"*",label="Clus NN")
+    ax1.plot(xx,density_rand,"o",color="orange",label="Rand NN",markersize=3)
+    yy = np.array([1.4 if item<0.5+final_t else 1.0 for item in xx])
+    ax1.plot(xx,yy,color="r",label='exact')
+    ax1.legend(loc="upper right")
+    fig1.savefig(f"{savepath}/density(t={final_t}).png")
+
+    fig2,ax2 = plt.subplots(1)
+    ax2.set_title(f"velocity(t={final_t})")
+    ax2.set_xlabel('x')
+    ax2.set_ylabel('velocity')
+    ax2.plot(xx,velocity_clus,"*",label="Clus NN")
+    ax2.plot(xx,velocity_rand,"o",color="orange",label="Rand NN",markersize=3)
+    ax2.plot(xx,np.ones(len(xx)),color="r",label="exact")
+    ax2.legend(loc="upper right")
+    ax2.axis(ymin=0.95,ymax=1.05)
+    fig2.savefig(f"{savepath}/velocity(t={final_t}.png")
+
+    fig3,ax3 = plt.subplots(1)
+    ax3.set_title(f"pressure(t={final_t})")
+    ax3.set_xlabel('x')
+    ax3.set_ylabel('pressure')
+    ax3.plot(xx,pressure_clus,"*",label="Clus NN")
+    ax3.plot(xx,pressure_rand,"o",color="orange",label="Rand NN",markersize=3)
+    ax3.plot(xx,np.ones(len(xx)),color="r",label="exact")
+    ax3.legend(loc="upper right")
+    ax3.axis(ymin=0.95,ymax=1.05)
+    fig3.savefig(f"{savepath}/pressure(t={final_t}).png")
+
+t = np.linspace(tmin,tmax,1001)
+x = np.linspace(xmin,xmax,101)
+t,x = np.meshgrid(t,x)
+t = t.reshape(-1, 1)
+x = x.reshape(-1, 1)
+TX = np.c_[t,x]
+
+u_clus,r_clus = pinn_clus.predict(t,x)
+u_rand,r_rand = pinn_rand.predict(t,x)
+u = [u_clus,u_rand]
+
+plot_final(TX,u,tmax)
